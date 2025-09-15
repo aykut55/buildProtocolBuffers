02858e2db816bd2f947e1c28cc0c7dab10e90e90
Aykut <aykut.tokatli@gmail.com>
2025-09-15 16:46:38 +0300

    - test projesi eklendi

---
+++ protobuf321InstallationTest/GoogleProtobufInstallTest/GoogleProtobufInstallTest/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <fstream>
+#include "person.pb.h"              // senin .proto’dan ürettiğin
+#include <google/protobuf/message.h> // Protobuf kütüphanesinden
+
+int main() {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    // --- Serialize ---
+    Person person;
+    person.set_name("Aykut Tokatlı");
+    person.set_id(42);
+    person.set_email("aykut@example.com");
+
+    std::ofstream out("person.bin", std::ios::binary);
+    if (!person.SerializeToOstream(&out)) {
+        std::cerr << "Failed to write person." << std::endl;
+        return -1;
+    }
+    out.close();
+
+    // --- Deserialize ---
+    Person person2;
+    std::ifstream in("person.bin", std::ios::binary);
+    if (!person2.ParseFromIstream(&in)) {
+        std::cerr << "Failed to read person." << std::endl;
+        return -1;
+    }
+    in.close();
+
+    std::cout << "Name : " << person2.name() << "\n"
+        << "Id   : " << person2.id() << "\n"
+        << "Email: " << person2.email() << std::endl;
+
+    google::protobuf::ShutdownProtobufLibrary();
+    return 0;
+}
+
---
+++ protobuf321InstallationTest/create_proto.bat
@@ -0,0 +1,33 @@
+﻿@echo off
+setlocal
+
+:: Varsayılan build türü Release
+set BUILD_TYPE=Debug
+
+:: Eğer parametre verdiysek (debug / release), BUILD_TYPE ona göre ayarlanır
+if /I "%1"=="debug" set BUILD_TYPE=Debug
+if /I "%1"=="release" set BUILD_TYPE=Release
+
+echo Using build type: %BUILD_TYPE%
+
+:: Protoc yolu
+set PROTOC=C:\protobuf\protobuf321\build\%BUILD_TYPE%\protoc.exe
+
+:: Proto kaynak dosyası
+set PROTO_SRC=C:\protobuf\protobuf321InstallationTest\GoogleProtobufInstallTest\GoogleProtobufInstallTest\src\person\person.proto
+
+:: Çıktı dizini (main.cpp ile aynı dizin)
+set PROTO_DST=C:\protobuf\protobuf321InstallationTest\GoogleProtobufInstallTest\GoogleProtobufInstallTest
+
+:: Protoc çağrısı
+echo Running: "%PROTOC%" --cpp_out="%PROTO_DST%" "%PROTO_SRC%"
+"%PROTOC%" --proto_path="%PROTO_DST%" --cpp_out="%PROTO_DST%" "%PROTO_SRC%"
+
+
+if %ERRORLEVEL% NEQ 0 (
+    echo Error: protoc failed!
+    exit /b %ERRORLEVEL%
+)
+
+echo Done. Files generated in: %PROTO_DST%
+pause
